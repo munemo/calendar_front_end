02e9d644e361ca895d1c8862f6098b8550f747c5
johnchimbani <john.chimbani@pocktor.com>
2020-12-23 11:01:03 +0100

    Adding Scheduler component and scheduler css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
+import React, { Component } from "react";
 //import Login from "./components/Login";
 //import Register from "./components/Register";
-import Home from "./components/Home";
+//import Home from "./components/Home";
+import "../src/css/Scheduler/scheduler.css";
+import Scheduler from "./components/Scheduler";
 
-
+const data = [
+  {
+    start_date: "2020-01-04 6:00",
+    end_date: "2020-07-12",
+    text: "Event 1",
+    id: 1,
+  },
+  {
+    start_date: "2020-01-04 6:00",
+    end_date: "2020-07-12",
+    text: "Event 2",
+    id: 2,
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="scheduler-container">
       
-          <Home />
+        <Scheduler events={data}/>
           
       
-      </header>
+      </div>
     </div>
   );
 }
